79e1b9f87733821cdc72ae67601c94c5f4e46ddf
Amit Kumar <amiku.cse@gmail.com>
2020-04-10 17:58:49 +0530

    Added AG-Grid Large Data Set and scroll to end
    1. Large Data set
    2. Scroll To end
    3. How to manage change in data.
    4. Maintaining scroll to end on change in Data

--- Grid Properties required.md
+++ Grid Properties required.md
@@ -1,11 +1,14 @@
 ##Properties required for office project
 
-* Handle very large amount of data. 20 rows * 10000 columns
-* virtual scroll horizontally.
-  * Programmatically reach to last column of the grid. and it should be decent experience
+### To investigate
 * custom style cells.
 * Fixed
   * first row.
   * last column.
 * Programmetically highlight a particular column of the grid and go to that column of the grid.
 
+
+### Completed
+* Handle very large amount of data. 20 rows * 10000 columns
+* virtual scroll horizontally.
+  * Programmatically reach to last column of the grid. and it should be decent experience
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hello-ag-grid';
+  gridOptions;
+  lastColIndex = 0;
 
-  columnDefs = [
-    {headerName: 'Make', field: 'make' },
-    {headerName: 'Model', field: 'model' },
-    {headerName: 'Price', field: 'price'}
-  ];
+  constructor() {
+    this.gridOptions = {
+      ...this.createData(20, 3600),
+      rowSelection: 'single'
+    };
+  }
 
-  rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 }
-  ];
+  ngOnInit(): void {
+
+  }
+
+  goToLastColumn = () => {
+    const lastColKey = 'col-' + this.lastColIndex;
+    this.gridOptions.api.ensureColumnVisible(lastColKey);
+  }
+
+  buttonClicked = () => {
+    this.goToLastColumn();
+  }
+
+  updateData = () => {
+    const newGridData = this.createData(20, 4200, 10);
+    this.gridOptions.api.setRowData(newGridData.rowData );
+    this.gridOptions.api.setColumnDefs(newGridData.columnDefs );
+    this.goToLastColumn();
+  }
+
+  createData = (rowCount: number, colCount: number, offset: number = 0) => {
+    this.lastColIndex = colCount - 1;
+    const rowData = [];
+    for (let row = 0; row < rowCount; row++) {
+      const obj = {};
+      for (let propertyIndex = 0; propertyIndex < colCount; propertyIndex++) {
+        obj['col-' + propertyIndex] = 'row-' + row + ' col-' + propertyIndex + '-' + offset;
+      }
+      rowData.push(obj);
+    }
+
+    const columnDefs: any[] = [];
+    for (let propertyIndex = 0; propertyIndex < colCount; propertyIndex++) {
+      const col = {
+        headerName: 'Column-' + propertyIndex,
+        field: 'col-' + propertyIndex
+      };
+      columnDefs.push(col);
+    }
+    return {
+      rowData,
+      columnDefs
+    };
+  }
 }
